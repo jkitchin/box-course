ac09f7e275424ca0ea34f8fec2d4a2f3775618d6
John Kitchin <jkitchin@andrew.cmu.edu>
2013-11-22 18:56:57 -0500

    I added code here to read the config file, and find it from anywhere in a box course.

--- box_course/__init__.py
+++ box_course/__init__.py
@@ -1,1 +1,20 @@
 __version__ = '0.1'
+
+import os
+
+# load the config file, looking recursively up
+dir, last = os.getcwd(), True
+cfile = os.path.join(dir, 'box_course_config.py')
+
+if os.path.exists(cfile):
+    execfile(cfile)
+else:
+
+    while last:  #the last dir is empty at the root of the file system
+        dir, last = os.path.split(dir)
+        cfile = os.path.join(dir, 'box_course_config.py')
+        if os.path.exists(cfile):
+            execfile(cfile)
+            break
+
+
